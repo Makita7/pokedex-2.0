82b6485d389a239a682d7fc45b9be42202326851
Sofía Makita <sophiamakita@gmail.com>
2025-06-17 23:52:47 -0300

    first commit

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
+import { server } from 'typescript'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    server: {
+      port: 5000,
+    },
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+  }),
+)
